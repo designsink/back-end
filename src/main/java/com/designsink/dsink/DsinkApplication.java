c6834431b86eade3858bc8010c07cc2f5a1d9dbd
jaemoon <woans5970@naver.com>
2025-06-04 14:16:30 +0900

    chore: init project

---
+++ src/main/java/com/designsink/dsink/DsinkApplication.java
@@ -0,0 +1,13 @@
+package com.designsink.dsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designsink/dsink/DsinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designsink.dsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
