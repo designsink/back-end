e184bebace5db051ff5ae7259289a47a8d0c7cd9
jaemoon <woans5970@naver.com>
2025-06-12 18:21:32 +0900

    refactor: dto, 필드명 수정

--- src/main/java/com/designsink/dsink/service/info/dto/common/MainPageInfoDto.java
+++ src/main/java/com/designsink/dsink/service/info/dto/common/MainPageInfoDto.java
@@ -1,17 +1,19 @@
-package com.designsink.dsink.service.info.dto.request;
+package com.designsink.dsink.service.info.dto.common;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MainPageInfoRequestDto {
+public class MainPageInfoDto {
 	private String title;
 	private String description;
 	private String storeName;
-	private String CEOName;
+	private String ceoName;
 	private String businessNumber;
 	private String email;
 	private String address;
