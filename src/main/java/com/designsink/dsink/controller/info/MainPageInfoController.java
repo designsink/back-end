e71c072194a849767439a058bcbf379feeabbeb6
jaemoon <woans5970@naver.com>
2025-06-12 16:31:18 +0900

    feat: 메인페이지 컨트롤러 추가

---
+++ src/main/java/com/designsink/dsink/controller/info/MainPageInfoController.java
@@ -0,0 +1,34 @@
+package com.designsink.dsink.controller.info;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.designsink.dsink.service.info.MainPageInfoService;
+import com.designsink.dsink.service.info.dto.request.MainPageInfoRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/main-page")
+@RestController
+@RequiredArgsConstructor
+public class MainPageInfoController {
+
+	private final MainPageInfoService mainPageInfoService;
+
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody MainPageInfoRequestDto requestDto) {
+		mainPageInfoService.create(requestDto);
+		return ResponseEntity.ok().body("메인페이지 정보 생성 성공");
+	}
+
+	@PutMapping("/{infoId}")
+	public ResponseEntity<String> update(@PathVariable Integer infoId, @RequestBody MainPageInfoRequestDto requestDto) {
+		mainPageInfoService.update(infoId, requestDto);
+		return ResponseEntity.ok().body("메인페이지 정보 수정 성공");
+	}
+}
