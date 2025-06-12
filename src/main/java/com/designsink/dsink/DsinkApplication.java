4d6a2ab14f212d7d2dbbe299100154eb523d4ba4
jaemoon <woans5970@naver.com>
2025-06-12 13:09:28 +0900

    feat: 통합 타임 엔티티 추가

--- src/main/java/com/designsink/dsink/DsinkApplication.java
+++ src/main/java/com/designsink/dsink/DsinkApplication.java
@@ -2,7 +2,9 @@ package com.designsink.dsink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DsinkApplication {
 
