3ab81c54491895b55c10422be9e1ce78a060f5b3
jaemoon <woans5970@naver.com>
2025-06-12 18:21:57 +0900

    feat: 메인페이지 조회 api 추가

--- src/main/java/com/designsink/dsink/controller/info/MainPageInfoController.java
+++ src/main/java/com/designsink/dsink/controller/info/MainPageInfoController.java
@@ -1,6 +1,7 @@
 package com.designsink.dsink.controller.info;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.designsink.dsink.service.info.MainPageInfoService;
-import com.designsink.dsink.service.info.dto.request.MainPageInfoRequestDto;
+import com.designsink.dsink.service.info.dto.common.MainPageInfoDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,14 +22,19 @@ public class MainPageInfoController {
 	private final MainPageInfoService mainPageInfoService;
 
 	@PostMapping
-	public ResponseEntity<String> create(@RequestBody MainPageInfoRequestDto requestDto) {
+	public ResponseEntity<String> create(@RequestBody MainPageInfoDto requestDto) {
 		mainPageInfoService.create(requestDto);
 		return ResponseEntity.ok().body("메인페이지 정보 생성 성공");
 	}
 
 	@PutMapping("/{infoId}")
-	public ResponseEntity<String> update(@PathVariable Integer infoId, @RequestBody MainPageInfoRequestDto requestDto) {
+	public ResponseEntity<String> update(@PathVariable Integer infoId, @RequestBody MainPageInfoDto requestDto) {
 		mainPageInfoService.update(infoId, requestDto);
 		return ResponseEntity.ok().body("메인페이지 정보 수정 성공");
+	}
+
+	@GetMapping("/{infoId}")
+	public ResponseEntity<MainPageInfoDto> find(@PathVariable Integer infoId) {
+		return ResponseEntity.ok().body(mainPageInfoService.find(infoId));
 	}
 }
