200f32c910f54c7bbc11bd4e0cab37c1a1ca33f2
jaemoon <woans5970@naver.com>
2025-06-16 15:29:28 +0900

    feat: 상품 목록 조회 dto 추가

--- src/main/java/com/designsink/dsink/service/product/dto/response/ProductsResponseDto.java
+++ src/main/java/com/designsink/dsink/service/product/dto/response/ProductsResponseDto.java
@@ -1,4 +1,15 @@
 package com.designsink.dsink.service.product.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductsResponseDto {
+	private Integer productId;
+	private String path;
 }
