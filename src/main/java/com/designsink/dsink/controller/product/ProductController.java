32e8429e2bf994b94187b661b1463688b30a4536
jaemoon <woans5970@naver.com>
2025-06-16 11:00:44 +0900

    feat: 상품 삭제 api 추가

--- src/main/java/com/designsink/dsink/controller/product/ProductController.java
+++ src/main/java/com/designsink/dsink/controller/product/ProductController.java
@@ -5,26 +5,37 @@ import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.designsink.dsink.entity.product.Product;
 import com.designsink.dsink.service.product.ProductService;
 import com.designsink.dsink.service.product.dto.ProductSaveRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/products")
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
 
 	private final ProductService productService;
 
-	@PostMapping(value = "/products", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<String> createProduct(@ModelAttribute ProductSaveRequestDto requestDto) {
 		productService.save(requestDto);
 		return ResponseEntity.ok().body("상품 등록 성공");
+	}
+
+	@DeleteMapping("/{productId}")
+	public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
+		return ResponseEntity.ok().body("<UNK> <UNK> <UNK>");
 	}
 
 	@GetMapping("/categories")
