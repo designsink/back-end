14131f1c2ea8cf76b433220e3d01583baf82a87b
jaemoon <woans5970@naver.com>
2025-06-16 15:31:35 +0900

    feat: 카테고리 별 상품 목록조회 쿼리 추가

--- src/main/java/com/designsink/dsink/repository/product/ProductItemRepository.java
+++ src/main/java/com/designsink/dsink/repository/product/ProductItemRepository.java
@@ -1,11 +1,16 @@
 package com.designsink.dsink.repository.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.designsink.dsink.entity.product.ProductItem;
+import com.designsink.dsink.entity.product.enums.ProductType;
 
 @Repository
 public interface ProductItemRepository extends JpaRepository<ProductItem, Integer> {
 	void deleteAllByProductId(Integer productId);
+
+	List<ProductItem> findAllByCategory(ProductType category);
 }
