f00cd877f5c94615b0311bf6403e2239d5b718c9
jaemoon <woans5970@naver.com>
2025-06-16 15:31:43 +0900

    feat: 상품 목록조회 쿼리 추가

--- src/main/java/com/designsink/dsink/repository/product/ProductRepository.java
+++ src/main/java/com/designsink/dsink/repository/product/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.designsink.dsink.repository.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.designsink.dsink.entity.product.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	@Query("SELECT DISTINCT p FROM product p WHERE p.isDeleted=false ORDER BY p.createdAt DESC")
+	List<Product> findDistinctById();
 }
