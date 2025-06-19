2a0aebba9fcfa4a25d41402fadec7e132239e599
jaemoon <woans5970@naver.com>
2025-06-19 19:12:01 +0900

    chore: 엑세스 토큰 dto 추가

---
+++ src/main/java/com/designsink/dsink/infra/auth/dto/AccessTokenDto.java
@@ -0,0 +1,15 @@
+package com.designsink.dsink.infra.auth.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class AccessTokenDto {
+	private String accessToken;
+}
