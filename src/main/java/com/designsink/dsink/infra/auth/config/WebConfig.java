6bbbe5e1d0edf0affa892d5457306d71db65fa81
jaemoon <woans5970@naver.com>
2025-06-24 21:11:30 +0900

    refactor: 허용 url 변경

--- src/main/java/com/designsink/dsink/infra/auth/config/WebConfig.java
+++ src/main/java/com/designsink/dsink/infra/auth/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "https://jaemoon99.site")
+			.allowedOrigins("http://localhost:3000", "https://dsink.kr")
 			.allowedMethods("*")
 			.allowedHeaders("*")
 			.allowCredentials(true);
