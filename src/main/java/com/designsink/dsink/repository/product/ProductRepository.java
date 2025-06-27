56e20b8d863dde011826f95518020e772291ebab
jaemoon <woans5970@naver.com>
2025-06-27 12:19:54 +0900

    refactor: 페이지네이션 쿼리로 변경

--- src/main/java/com/designsink/dsink/repository/product/ProductRepository.java
+++ src/main/java/com/designsink/dsink/repository/product/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.designsink.dsink.repository.product;
 
-import java.util.List;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,12 +11,11 @@ import com.designsink.dsink.entity.product.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	@Query("""
-    SELECT DISTINCT p
-    FROM product p
-    JOIN p.productItems pi
-    WHERE p.isDeleted = false
-      AND pi.category <> com.designsink.dsink.entity.product.enums.ProductType.MAIN
-    ORDER BY p.createdAt DESC
+        SELECT DISTINCT p
+          FROM product p
+          JOIN p.productItems pi
+         WHERE p.isDeleted = false
+           AND pi.category <> com.designsink.dsink.entity.product.enums.ProductType.MAIN
     """)
-	List<Product> findActiveProductsExcludingMainCategory();
+	Slice<Product> findActiveProductsExcludingMainCategory(Pageable pageable);
 }
