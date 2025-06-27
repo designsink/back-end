5a8774110323a24dffcba76567d906ca37d684c3
jaemoon <woans5970@naver.com>
2025-06-27 12:21:13 +0900

    refactor: 클래스며 변경

--- src/main/java/com/designsink/dsink/service/product/dto/response/ProductsSliceResponseDto.java
+++ src/main/java/com/designsink/dsink/service/product/dto/response/ProductsSliceResponseDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductsResponseDto {
+public class ProductsSliceResponseDto {
 	private Integer productId;
 	private String path;
 }
