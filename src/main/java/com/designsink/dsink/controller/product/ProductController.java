79bf68d80dfa570244e906b520a38d64ae72ffba
jaemoon <woans5970@naver.com>
2025-07-02 12:04:50 +0900

    refactor: 순서 수정 api 추가

--- src/main/java/com/designsink/dsink/controller/product/ProductController.java
+++ src/main/java/com/designsink/dsink/controller/product/ProductController.java
@@ -8,14 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.designsink.dsink.entity.product.enums.ProductType;
 import com.designsink.dsink.service.product.ProductService;
+import com.designsink.dsink.service.product.dto.request.ProductModifyRequestDto;
 import com.designsink.dsink.service.product.dto.request.ProductSaveRequestDto;
 import com.designsink.dsink.service.product.dto.response.ProductDetailResponseDto;
 import com.designsink.dsink.service.product.dto.response.ProductsResponseDto;
@@ -58,5 +61,13 @@ public class ProductController {
 	@GetMapping("/categories")
 	public ResponseEntity<List<Map<String, String>>> getCategories() {
 		return ResponseEntity.ok().body(productService.getCategories());
+	}
+
+	@PatchMapping()
+	public ResponseEntity<String> updateProductSequences(
+		@RequestParam("category") ProductType productType,
+		@RequestBody List<ProductModifyRequestDto> requestDtos) {
+		productService.modifySequences(productType, requestDtos);
+		return ResponseEntity.ok().body("수정 완료");
 	}
 }
